21f05146a56a25f276ae58beadb671a6e3090b0a
michaelmcnamara <michael.mcnamara@guardian.co.uk>
2013-09-24 22:36:42 +0100

    more stuff

--- src/simplebattleships/Grid.java
+++ src/simplebattleships/Grid.java
@@ -10,8 +10,8 @@ import java.awt.*;
  * To change this template use File | Settings | File Templates.
  */
 public class Grid {
-    String oneDGrid[];
-    Ship shipOne;
+    private String oneDGrid[];
+    private Ship shipOne;
 
     Grid(){
            this.oneDGrid = new String[7];
@@ -35,15 +35,56 @@ public class Grid {
 
     }
 
+    public int getlength() {
+        return oneDGrid.length;
+    }
+
+    public boolean istargetdestroyed(int guess) {
+        String result = oneDGrid[guess];
+        if (result == ""){
+           System.out.println("Sir, lookout reports a miss. \n");
+           return false;}
+       else {
+            System.out.println("Sir, lookout reports a hit! \n");
+            oneDGrid[guess] = "";
+            return anyMoreTargets();
+            }
+
+       }
+
+    private boolean anyMoreTargets() {
+        for(int i=0; i<oneDGrid.length; i++){
+            if(oneDGrid[i] != "")
+                return true;
+        }
+        return false;  //To change body of created methods use File | Settings | File Templates.
+    }
+
+
     @Override
     public String toString() {
-        String grid = "";
-        for(int i=0; i < (oneDGrid.length-1); i++){
-            grid.concat(oneDGrid[i] + ", ");
+        String grid1 = oneDGrid[0];
+        String grid2 = oneDGrid[1];
+        String grid3 = oneDGrid[2];
+        String grid4 = oneDGrid[3];
+        String grid5 = oneDGrid[4];
+        String grid6 = oneDGrid[5];
+        String grid7 = oneDGrid[6];
+
+//        String grid =(grid1 + "," + grid1 + "," + grid2 + "," + grid3 + "," + grid4 + "," + grid5 + "," + grid6 + "," + grid7 );
+
+        String grid = "Grid 0 = " + oneDGrid[0];
+        for(int i=1; i < (oneDGrid.length-1); i++){
+            grid = grid.concat(", Grid "+ i + ": " + oneDGrid[i]);
         }
-        grid.concat(oneDGrid[oneDGrid.length]);
-        return grid;    //To change body of overridden methods use File | Settings | File Templates.
+        return grid;
     }
+
+
+//    public String fireAtGuess(String guess) {
+//        int index = guess.
+//        return null;  //To change body of created methods use File | Settings | File Templates.
+//    }
 
 
 }
--- src/simplebattleships/Runner.java
+++ src/simplebattleships/Runner.java
@@ -1,5 +1,7 @@
 package simplebattleships;
 
+import java.io.IOException;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Michael
@@ -8,9 +10,27 @@ package simplebattleships;
  * To change this template use File | Settings | File Templates.
  */
 public class Runner {
-    public static void main(String[] Args){
-        Grid grid = new Grid();
-        String gridString = grid.toString();
+    static Grid grid;
+
+
+    public static void main(String[] Args) throws IOException {
+        String guess="1";
+        Integer numericGuess = Integer.valueOf(guess);
+
+        Boolean targetDestroyed = false;
+        while(!targetDestroyed){
+            Grid grid = new Grid();
+            System.out.println("Captain!, where should we fire? available grid targets are: Grid 0 to Grid " + grid.getlength() + "\n");
+            ;
+ //            guess = System.in.toString();
+            System.out.println("Yes Sir! Fire on Grid " + guess + "\n Firing on Grid " + guess + "\n BOOM! \n");
+            targetDestroyed = grid.istargetdestroyed(numericGuess);
+            numericGuess = numericGuess+1;
+        }
+        System.out.println("Captain! Lookout reports smoke! Sonar reports ship sunk We got him sir!\n");
+
+
+//        System.out.println("grid contains: " + grid.toString());
     }
 
 
